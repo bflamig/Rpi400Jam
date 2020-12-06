4e1da736c7a3e49d37daba730abcddba23241776
Bryan Flamig <69734510+bflamig@users.noreply.github.com>
2020-12-06 06:11:38 -0700

    Generalized the install script a little

--- jamulus_install.sh
+++ jamulus_install.sh
@@ -4,7 +4,7 @@ apt-get -y install jackd2
 apt-get -y install build-essential qtdeclarative5-dev qt5-default qttools5-dev-tools libjack-jackd2-dev
 cd /home/pi
 rm -r -f jamulus
-git clone -b r3_6_1 https://github.com/corrados/jamulus.git
+git clone -b $1 https://github.com/corrados/jamulus.git
 cd /home/pi/jamulus
 qmake Jamulus.pro
 make clean
