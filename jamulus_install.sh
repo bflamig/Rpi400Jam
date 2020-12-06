4c7527d4e5480ed674fb9101454e383fe6fd7a3d
Bryan Flamig <69734510+bflamig@users.noreply.github.com>
2020-12-06 06:27:09 -0700

    Update jamulus_install.sh

--- jamulus_install.sh
+++ jamulus_install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 cd /home/pi
-apt-get -y install jackd2
-apt-get -y install build-essential qtdeclarative5-dev qt5-default qttools5-dev-tools libjack-jackd2-dev
+sudo apt-get -y install jackd2
+sudo apt-get -y install build-essential qtdeclarative5-dev qt5-default qttools5-dev-tools libjack-jackd2-dev
+sudo apt-get patchage qjackctl
 cd /home/pi
 rm -r -f jamulus
 git clone -b $1 https://github.com/corrados/jamulus.git
 cd /home/pi/jamulus
 qmake Jamulus.pro
 make clean
-make install
-adduser -system --no-create-home jamulus
-adduser jamulus audio
+sudo make install
+sudo adduser -system --no-create-home jamulus
+sudo adduser jamulus audio
 cd /home/pi
 
 
