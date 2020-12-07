d01bdad36844639038d4d240c4eb93a8b9696b21
Bryan Flamig <69734510+bflamig@users.noreply.github.com>
2020-12-06 17:40:20 -0700

    Generalized home directory path

--- jack_setup.sh
+++ jack_setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "/usr/bin/jackd -T -P95 -p16 -t2000 -d alsa -dhw:$1 -p 64 -n 2 -r 48000" > /home/pi/.jackdrc
+echo "/usr/bin/jackd -T -P95 -p16 -t2000 -d alsa -dhw:$1 -p 64 -n 2 -r 48000" > ~/.jackdrc
