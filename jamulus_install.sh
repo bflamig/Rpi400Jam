c704d2965fb3025aa41af9079c6a82eacf0cb568
Bryan Flamig <69734510+bflamig@users.noreply.github.com>
2020-12-06 17:39:34 -0700

    Generalized home directory path

--- jamulus_install.sh
+++ jamulus_install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-cd /home/pi
+cd ~/
 sudo apt-get -y install jackd2
 sudo apt-get -y install build-essential qtdeclarative5-dev qt5-default qttools5-dev-tools libjack-jackd2-dev
 sudo apt-get patchage qjackctl
-cd /home/pi
+cd ~/
 rm -r -f jamulus
 git clone -b $1 https://github.com/corrados/jamulus.git
-cd /home/pi/jamulus
+cd ~/jamulus
 qmake Jamulus.pro
 make clean
 sudo make install
 sudo adduser -system --no-create-home jamulus
 sudo adduser jamulus audio
-cd /home/pi
-echo "*** You MUST reboot your machine for these changes to take effect ***"
+cd ~/
+echo "*** WARNING: You MUST reboot your machine for these changes to take effect ***"
 
 
 
