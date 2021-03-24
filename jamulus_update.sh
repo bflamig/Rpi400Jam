b394b8547688df57586d369583fc1c51a8b90d7b
Bryan Flamig <69734510+bflamig@users.noreply.github.com>
2021-03-24 10:28:01 -0700

    Update jamulus_update.sh
    Revised the script to use the new path to the jamulus software in github, after 3_7_0

--- jamulus_update.sh
+++ jamulus_update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 cd ~/
 rm -r -f jamulus
-git clone -b $1 https://github.com/corrados/jamulus.git
+# Uncomment the line below for jamulus versions before 3_7_0
+# git clone -b $1 https://github.com/corrados/jamulus.git
+# Uncomment the line below for jamulus versions on or after 3_7_0
+git clone -b $1 https://github.com/jamulussoftware/jamulus.git
 cd ~/jamulus
 qmake Jamulus.pro
 make clean
